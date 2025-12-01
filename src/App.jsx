1fc5da7d74260e2d1a6273d7c963156fc329eb67
Ondřej Pelcl <ondrejpel@seznam.cz>
2025-12-01 01:46:53 +0100

    Add placeholder text

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={"h-screen flex justify-center items-center flex-col"}>
+      <h1 className={""}>Working on it</h1>
+      <h1 className={""}>Like really..</h1>
+    </div>
   )
 }
 
