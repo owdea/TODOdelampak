4fad6af553fa639f56dfd1ba7d9062fe211b42a1
Ondřej Pelcl <ondrejpel@seznam.cz>
2025-12-01 22:23:04 +0100

    Add Google Auth Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import GoogleAuthButton from "./components/googleAuthButton.jsx";
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
     <div className={"h-screen flex justify-center items-center flex-col"}>
       <h1 className={""}>Working on it</h1>
       <h1 className={""}>Like really..</h1>
+        <GoogleAuthButton/>
     </div>
   )
 }
